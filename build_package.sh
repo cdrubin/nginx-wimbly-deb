f282644e8270fa50d22f0ffa6f8a771ba59b1830
cd rubin <daniel@rubin.za.net>
2015-09-25 21:45:53 -0400

    yip

--- build_package.sh
+++ build_package.sh
@@ -4,7 +4,7 @@
 
 cd BUILD
 
-tar -xzvf ngx_openresty-$RESTY_VERSION.tar.gz
-mv ngx_openresty-$RESTY_VERSION nginx-wimbly-$RESTY_VERSION
+tar -xzvf ngx_openresty-$NGINX_VERSION.tar.gz
+mv ngx_openresty-$NGINX_VERSION nginx-wimbly-$RESTY_VERSION
 tar -xzvf luarocks-wimbly-$LUAROCKS_VERSION.tar.gz
 mv luarocks-wimbly-$LUAROCKS_VERSION nginx-benchmark-$RESTY_VERSION/bundle
