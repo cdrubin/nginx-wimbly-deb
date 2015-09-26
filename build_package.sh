21b517c61c95df44f7acee8394afc6779d031367
cd rubin <daniel@rubin.za.net>
2015-09-25 21:39:18 -0400

    cleaning up

--- build_package.sh
+++ build_package.sh
@@ -2,3 +2,9 @@
 
 . ./INCLUDE/VARS
 
+cd BUILD
+
+tar -xzvf ngx_openresty-$RESTY_VERSION.tar.gz
+mv ngx_openresty-$RESTY_VERSION nginx-wimbly-$RESTY_VERSION
+tar -xzvf luarocks-wimbly-$LUAROCKS_VERSION.tar.gz
+mv luarocks-wimbly-$LUAROCKS_VERSION nginx-benchmark-$RESTY_VERSION/bundle
